9ff8764ffb0c581eef3662f6eae313075508da55
Andrej Leban <andrejleban@gmail.com>
2022-05-04 18:58:40 -0700

    case-sensitive

--- regression2.R
+++ regression2.R
@@ -13,17 +13,17 @@ hist(data2$nmbrlang)
 data2 = data2[data2$nmbrlang > 0,]
 data2$lnnmbrlang = log(data2$nmbrlang)
 
-colnames(data2) = c('virtCode', 'countryCode', 'climate', 'soil', 
-                   'sdClimate', 'sdSoil', 'seaDist', 'avgElev', 'avgPrecip', 
+colnames(data2) = c('virtCode', 'countryCode', 'climate', 'soil',
+                   'sdClimate', 'sdSoil', 'seaDist', 'avgElev', 'avgPrecip',
                    'avgTemp', 'sdElev', 'waterArea', 'suitableCells',
-                   'avgSuit', 'sdSuit', 'popDens95', 'range', 'area', 
-                   'withinCountry', 'numCountry', 'migrationDist', 'lnLang', 
+                   'avgSuit', 'sdSuit', 'popDens95', 'range', 'area',
+                   'withinCountry', 'numCountry', 'migrationDist', 'lnLang',
                    'totalPop95', 'absLat', 'tropics', 'erange_gecon',
-                   'lnArea', 'lnAvgPop95', 'pctIndigenous', 'diffAvgElev', 
-                   'diffAvgPrecip', 'diffAvgTemp', 'diffAvgSuit', 
+                   'lnArea', 'lnAvgPop95', 'pctIndigenous', 'diffAvgElev',
+                   'diffAvgPrecip', 'diffAvgTemp', 'diffAvgSuit',
                    'overlap', 'numLang')
 
 data2 %>% filter(totalPop95 >= 3000) %>%
-  select(c('lnLang', 'sdElev', 'sdSuit', 'avgSuit', 'avgElev', 'absLat', 
-            'avgPrecip', 'avgTemp', 'area', 'seaDist', 'waterArea', 
+  select(c('lnLang', 'sdElev', 'sdSuit', 'avgSuit', 'avgElev', 'absLat',
+            'avgPrecip', 'avgTemp', 'area', 'seaDist', 'waterArea',
             'migrationDist', 'withinCountry', 'numCountry', 'popDens95')) %>% cor()
