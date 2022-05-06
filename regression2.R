4e7b48b306a313f82abcf1c22553aea04ac4b149
Isaac Schmidt <ischmidt20@berkeley.edu>
2022-05-05 17:24:00 -0700

    update import of new regression 2 data

--- regression2.R
+++ regression2.R
@@ -4,24 +4,23 @@ library(tidyverse)
 
 data2 = read.dta13('data_raw/Tables4-7b.dta')
 greg = read.csv('greg.csv')
-colnames(greg) = c('uniq_cnt25', 'nmbrlang')
+colnames(greg) = c('uniq_cnt25', 'number_suit_valid25', 'nmbrlang')
 
-data2 = data2 %>% select(-c('nmbrlang')) %>% merge(greg, by = 'uniq_cnt25')
+data2 = data2 %>% select(-c('nmbrlang', 'number_suit_valid25')) %>% merge(greg, by = 'uniq_cnt25')
 
 hist(data2$nmbrlang)
 
-data2 = data2[data2$nmbrlang > 0,]
 data2$lnnmbrlang = log(data2$nmbrlang)
 
 colnames(data2) = c('virtCode', 'countryCode', 'climate', 'soil',
                    'sdClimate', 'sdSoil', 'seaDist', 'avgElev', 'avgPrecip',
-                   'avgTemp', 'sdElev', 'waterArea', 'suitableCells',
-                   'avgSuit', 'sdSuit', 'popDens95', 'range', 'area',
-                   'withinCountry', 'numCountry', 'migrationDist', 'lnLang',
-                   'totalPop95', 'absLat', 'tropics', 'erange_gecon',
-                   'lnArea', 'lnAvgPop95', 'pctIndigenous', 'diffAvgElev',
+                   'avgTemp', 'sdElev', 'waterArea', 'avgSuit', 'sdSuit', 
+                   'popDens95', 'range', 'area', 'withinCountry', 
+                   'numCountry', 'migrationDist', 'lnLang', 'totalPop95',
+                   'absLat', 'tropics', 'erange_gecon', 'lnArea', 
+                   'lnAvgPop95', 'pctIndigenous', 'diffAvgElev',
                    'diffAvgPrecip', 'diffAvgTemp', 'diffAvgSuit',
-                   'overlap', 'numLang')
+                   'overlap', 'suitableCells', 'numLang')
 
 data2 %>% filter(totalPop95 >= 3000) %>%
   select(c('lnLang', 'sdElev', 'sdSuit', 'avgSuit', 'avgElev', 'absLat',
